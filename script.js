4e75de9124c4951305a2ed2204a956bcf37b4ad1
sakshidalalsd <139738505+sakshidalalsd@users.noreply.github.com>
2023-09-21 12:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If requi
 document.addEventListener("DOMContentLoaded",() =>{
-   document.body.innerText="Dom load success ";
+   document.body.innerText="DOM load success ";
 });
